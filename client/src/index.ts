1728c22bd3e98e70cae3083b0ea6c4ce62de7987
André Papoti <andrepapoti@gmail.com>
2022-07-08 00:32:35 -0300

    feat: add  client assynchronous requests

--- client/src/index.ts
+++ client/src/index.ts
@@ -7,8 +7,9 @@ type MyRequest = {
 type RequestData = {
   id: number;
   request: any;
-  sendTimeStamp: number;
+  sendTimeStamp?: number;
   receivedTimeStamp?: number;
+  responseTime?: number;
   response?: any;
 };
 
@@ -22,45 +23,81 @@ const createRequest = (body: number): MyRequest => {
     headers: {
       "content-type": "application/json;charset=UTF-8",
     },
-    body: body % 10 === 0 ? `${body}` : undefined,
+    body: body % 10 === 0 ? `${body + 1}` : undefined,
   };
 
   return x;
 };
 
 const logResponse = (data: RequestData): void => {
-  console.log("## Success\n", data);
+  switch (data.request.method) {
+    case "GET":
+      console.log("## GET: ", data.response);
+      break;
+    case "POST":
+      console.log("## POST OK:");
+      break;
+    default:
+      console.log("!! Unknown method");
+  }
+
+  if (data.receivedTimeStamp !== undefined && data.sendTimeStamp !== undefined)
+    console.log(
+      "Response time: ",
+      data.receivedTimeStamp - data.sendTimeStamp,
+      "ms\n"
+    );
+  else console.log("Invalid time stamp");
 };
 
 const executeRequests = async (): Promise<void> => {
   const numberOfRequests = 100;
-  const delayBetweenRequests = 1000;
-  const date = new Date();
+  const delayBetweenRequests = 400;
   const requests: RequestData[] = [];
-  for (let i = 0; i < 500; i += 1) {
+  for (let i = 0; i < numberOfRequests; i += 1) {
     requests.push({
       id: i,
       request: createRequest(i),
-      sendTimeStamp: date.getUTCMilliseconds(),
     });
   }
 
+  const promiseArray: Promise<void>[] = [];
+
   // eslint-disable-next-line no-undef
   for (let i = 0; i < numberOfRequests; i += 1) {
-    console.log("sending", i);
-    fetch("http://34.121.242.204:5000", requests[i].request)
-      .then((response) => {
-        requests[i].receivedTimeStamp = date.getUTCMilliseconds();
-        requests[i].response = response;
-        logResponse(requests[i]);
-      })
-      .catch((error) => {
-        console.error("Error on request", i, error, requests[i]);
-      });
+    requests[i].sendTimeStamp = performance.now();
+    promiseArray.push(
+      fetch("http://34.121.242.204:5000", requests[i].request)
+        .then((response) => {
+          requests[i].receivedTimeStamp = performance.now();
+
+          return response.text();
+        })
+        .then((data) => {
+          requests[i].response = data;
+          logResponse(requests[i]);
+        })
+        .catch((error) => {
+          console.error("Error on request", i, error, requests[i]);
+        })
+    );
 
     // eslint-disable-next-line no-await-in-loop
     await sleep(delayBetweenRequests);
   }
+
+  await Promise.all(promiseArray);
+
+  const totalTime = requests.reduce((acc, request) => {
+    if (request.sendTimeStamp === undefined) return acc;
+    if (request.receivedTimeStamp === undefined) return acc;
+    const responseTime: number =
+      request.receivedTimeStamp - request.sendTimeStamp;
+    console.log(responseTime);
+    return acc + responseTime;
+  }, 0);
+
+  console.log("# Average response time: ", totalTime / numberOfRequests, "ms");
 };
 
 executeRequests();
